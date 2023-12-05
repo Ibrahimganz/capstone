71b88e78f42d80b1308c3e66962213a7d0157689
Ibrahimganz <90731262+Ibrahimganz@users.noreply.github.com>
2023-12-05 11:31:21 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,25 +2,32 @@ const generateToken = () => {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   };
   
-  const register = (req, res) => {
-    const { username, password } = req.body;
+  const register = async (req, res) => {
+    try {
+      const { username, password } = req.body;
    
+      if (!username || !password) {
+        throw new Error('Username dan password harus diisi');
+      }
+   
+      const newUser = { username, password, token: null };
+   
+      if (username === 'user1') {
+        throw new Error('Username sudah digunakan');
+      }
+   
+      newUser.token = generateToken();
   
-    res.json({ success: true, message: 'Registrasi berhasil' });
+      res.json({ success: true, message: 'Registrasi berhasil', user: newUser });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
   };
   
-  const login = (req, res) => {
-    const { username, password } = req.body; 
-    const token = generateToken();
-  
-    res.json({ success: true, message: 'Login berhasil', token });
+  const login = (req, res) => { 
   };
   
-  const logout = (req, res) => {
-    const currentUser = req.currentUser;
-    currentUser.token = null;
-  
-    res.json({ success: true, message: 'Logout berhasil' });
+  const logout = (req, res) => { 
   };
   
   module.exports = { register, login, logout };
